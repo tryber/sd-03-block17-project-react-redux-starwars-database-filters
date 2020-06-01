3a243dc52da816841f84f6183791c176289e97d4
Lourenço <loulage@gmail.com>
2020-06-01 17:49:48 -0300

    Implemented apiReducer

---
+++ src/reducers/apiReducer.js
@@ -0,0 +1,37 @@
+import {
+  API_REQUEST,
+  API_RECIEVE_SUCCESS,
+  API_RECIVE_FAILURE,
+} from '../actions/apiAction';
+
+const INITIAL_STATE = {
+  data: [],
+  loading: false,
+};
+
+const apiReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case API_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case API_RECIEVE_SUCCESS:
+      return {
+        ...state,
+        data: [...action.data],
+        loading: false,
+      };
+    case API_RECIVE_FAILURE:
+      return {
+        ...state,
+        error: action.errorMessage,
+        loading: false,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default apiReducer;
