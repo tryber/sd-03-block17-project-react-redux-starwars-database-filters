990c13049af7a013c5d28e9332f13f43f31a8cf6
Lucas Werner <lucas.1werner@gmail.com>
2020-06-01 15:26:16 -0300

    API pronta

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Provider } from 'react-redux';
+import Table from './components/Table';
+import store from './store';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Provider store={store}>
+      <div class Name="App">
+        <Table />
+      </div>
+    </Provider>
   );
 }
 
