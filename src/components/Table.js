e432cf107ab6ed607a24fc63599c520d66f29828
Marco Barbosa <marco.meireles.b@gmail.com>
2020-06-02 16:50:49 -0300

    codeclimate rev2

--- src/components/Table.js
+++ src/components/Table.js
@@ -4,23 +4,23 @@ import PropTypes, { object } from 'prop-types';
 import './Table.css';
 
 const tableCreator = (obj) =>
-    <tr key={obj.name}>
-      <td>{obj.name}</td>
-      <td>{obj.rotation_period}</td>
-      <td>{obj.orbital_period}</td>
-      <td>{obj.diameter}</td>
-      <td>{obj.climate}</td>
-      <td>{obj.gravity}</td>
-      <td>{obj.terrain}</td>
-      <td>{obj.surface_water}</td>
-      <td>{obj.population}</td>
-      <td>
-        {obj.films.map((e) => e)}
-      </td>
-      <td>{obj.created}</td>
-      <td>{obj.edited}</td>
-      <td>{obj.url}</td>
-    </tr>;
+  <tr key={obj.name}>
+    <td>{obj.name}</td>
+    <td>{obj.rotation_period}</td>
+    <td>{obj.orbital_period}</td>
+    <td>{obj.diameter}</td>
+    <td>{obj.climate}</td>
+    <td>{obj.gravity}</td>
+    <td>{obj.terrain}</td>
+    <td>{obj.surface_water}</td>
+    <td>{obj.population}</td>
+    <td>
+      {obj.films.map((e) => e)}
+    </td>
+    <td>{obj.created}</td>
+    <td>{obj.edited}</td>
+    <td>{obj.url}</td>
+  </tr>;
 
 class Table extends React.Component {
   render() {
