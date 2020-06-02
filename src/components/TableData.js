4225244b093430d1399621c96ba5682bfeb86a04
Jafet Henrique <jafetguerra6@gmail.com>
2020-06-02 18:58:06 -0300

    cc

--- src/components/TableData.js
+++ src/components/TableData.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Table from './Table';
 
 const TableData = (props) => {
   const { dataSw } = props;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Provider } from 'react-redux';
 import App from './App';
 import store from './store/index';
 import * as serviceWorker from './serviceWorker';
