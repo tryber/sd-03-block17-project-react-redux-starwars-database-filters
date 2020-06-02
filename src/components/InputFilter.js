b66b2ce2f0f3fdf27825fd2a85dcd7852421d6a5
nato-x <alvares.renato@hotmail.com>
2020-06-02 18:26:35 -0300

    mapStateToProps of InputFilter workin, table head fixed

--- src/components/InputFilter.js
+++ src/components/InputFilter.js
@@ -4,13 +4,22 @@ import propTypes from 'prop-types';
 import { filterByName } from '../actions/textInputActions';
 
 const InputFilter = ({
-  dispatchInput,
-}) => <input data-testid="name-filter" onChange={(e) => dispatchInput((e.target.value))} />;
+  dispatchInput, value,
+}) => <input data-testid="name-filter" onChange={(e) => dispatchInput((e.target.value))} value={value} />;
+
 const mapDispatchToPros = (dispatch) => ({
   dispatchInput: (text) => dispatch(filterByName(text)),
 });
-export default connect(null, mapDispatchToPros)(InputFilter);
+
+const mapStateToProps = (state) => {
+  console.log(state);
+  return {
+    value: state.textFilterReducer.filters.filterByName.name,
+  };
+};
+export default connect(mapStateToProps, mapDispatchToPros)(InputFilter);
 
 InputFilter.propTypes = {
   dispatchInput: propTypes.func.isRequired,
+  value: propTypes.string.isRequired,
 };
--- src/components/TableHeaders.js
+++ src/components/TableHeaders.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
-const TableHeaders = ({ heads }) => <thead>{heads.map((head) => <th>{head}</th>)}</thead>;
+const TableHeaders = ({ heads }) => <tr>{heads.map((head) => <th>{head}</th>)}</tr>;
 export default TableHeaders;
 
 TableHeaders.propTypes = {
