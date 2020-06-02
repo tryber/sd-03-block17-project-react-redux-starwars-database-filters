cb4da1813dfceb1ba04e291e5f98407eb620f81a
nato-x <alvares.renato@hotmail.com>
2020-06-02 17:50:47 -0300

    CC fix

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import Table from './components/Table';
 import InputFilter from './components/InputFilter';
 import './App.css';
 
-const App = () => {
-    return (
-      <div className="App">
-        <InputFilter />
-        <Table />
-      </div>
-    )
-}
+const App = () => (
+  <div className="App">
+    <InputFilter />
+    <Table />
+  </div>
+);
 
 export default App;
--- src/components/InputFilter.js
+++ src/components/InputFilter.js
@@ -3,7 +3,9 @@ import { connect } from 'react-redux';
 import propTypes from 'prop-types';
 import { filterByName } from '../actions/textInputActions';
 
-const InputFilter = ({ dispatchInput }) => <input data-testid="name-filter" onChange={(e) => dispatchInput((e.target.value))} />;
+const InputFilter = ({
+  dispatchInput,
+}) => <input data-testid="name-filter" onChange={(e) => dispatchInput((e.target.value))} />;
 const mapDispatchToPros = (dispatch) => ({
   dispatchInput: (text) => dispatch(filterByName(text)),
 });
