d446d38d7aef8ab5a8bb51b11cad39d1f70996ba
Lourenço <loulage@gmail.com>
2020-06-02 15:44:17 -0300

    Implemented Provider on App component, to provide store to its children

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
 import Table from './components/Table';
+import store from './store/index';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Table />
-      </header>
+      <Provider store={store}>
+        <header className="App-header">
+          <Table />
+        </header>
+      </Provider>
     </div>
   );
 }
