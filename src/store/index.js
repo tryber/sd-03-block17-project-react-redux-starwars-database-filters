33ab595e9e5574a94193c7f7421d88881078e204
Hfreitas <hfreitas86@gmail.com>
2020-06-02 13:52:08 -0300

    implementando logger e redux devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,11 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import { logger } from 'redux-logger';
 import rootReducer from '../reducers/index';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk, logger)));
 
 export default store;
