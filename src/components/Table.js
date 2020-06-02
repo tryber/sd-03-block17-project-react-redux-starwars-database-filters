9f18ac85398f1256caa5b53b8e4645cc20f2bbce
Gabryel <gabryel.ryba@gmail.com>
2020-06-02 18:28:07 -0300

    Table component now displays data fetched from sw api

--- src/components/Table.js
+++ src/components/Table.js
@@ -16,17 +16,46 @@ class Table extends React.Component {
     fetchPlanets();
   }
 
+  renderTableHead() {
+    const { data } = this.props;
+    if (data.length === 0) {
+      return null;
+    }
+    return (
+      <thead>
+        <tr>
+          {Object.keys(data[0]).splice(0, 9).map((info) => (<th key={info}>{info}</th>))}
+        </tr>
+      </thead>
+    );
+  }
+
+  renderTableBody() {
+    const { data } = this.props;
+    return (
+      <tbody>
+        {data.map((row) => (
+          <tr key={row.name}>
+            {Object.values(row).splice(0, 9).map((value) => (<td key={value}>{value}</td>))}
+          </tr>
+        ))}
+      </tbody>
+    );
+  }
+
   render() {
     return (
-      <div>
-        <p>Table Component</p>
-      </div>
+      <table>
+        {this.renderTableHead()}
+        {this.renderTableBody()}
+      </table>
     );
   }
 }
 
 Table.propTypes = {
-  fetchPlanets: propTypes.isRequired,
+  fetchPlanets: propTypes.func.isRequired,
+  data: propTypes.arrayOf(propTypes.object).isRequired,
 };
 
 const mapStateToProps = (state) => ({
