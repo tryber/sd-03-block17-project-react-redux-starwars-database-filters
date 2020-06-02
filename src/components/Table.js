14f8fb1c6715b9f36233d74c0285cefb05d83147
Clayton Pereira <cpereiramt@gmail.com>
2020-06-02 19:08:11 -0400

    Salvando alteraçoes requisito 1

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 export class Table extends Component {
   renderTableBody() {
-    const { results } = this.props.value.data;
+    const { value: { data } } = this.props;
+    const { results } = data;
     return results.map((element) => (
       <tr>
         <td >{element.name}</td>
@@ -18,19 +20,25 @@ export class Table extends Component {
         <td >{element.films}</td>
         <td >{element.created}</td>
         <td >{element.edited}</td>
-        <td >{element.url}</td>
+        <td>{element.url}</td>
       </tr>
     ));
   }
 
   render() {
-    const { results } = this.props.value.data;
+    const { value: { data } } = this.props;
+    const { results } = data;
     const headers = ['name', 'rotation_period', 'orbital_period', 'diameter', 'climate', 'gravity', 'terrain', 'surface_water', 'population', 'films', 'created', 'edited', 'url'];
     return (
       <div>
         {results.length === 1
-          ? (<h1> {results[0]}</h1>) : (
-            <div> <p> Table </p>
+          ? (
+            <h1> 
+              {results[0]}
+            </h1>
+          ) : (
+            <div>
+              <p> Table </p>
               <table>
                 <thead>
                   <tr>
@@ -50,10 +58,16 @@ export class Table extends Component {
 
 const mapStateToProps = (state) => ({ value: state });
 
-Table.defaultsProps = {
+Table.propTypes = {
+  data: PropTypes.instanceOf(Object),
+  results: PropTypes.instanceOf(Array),
+  value: PropTypes.instanceOf(Array),
+};
+
+Table.defaultProps = {
   data: {},
   results: [],
-
+  value: [],
 };
 
 export default connect(mapStateToProps)(Table);
