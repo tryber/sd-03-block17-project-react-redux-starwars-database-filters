e027c816023bb9afc181e70d8b0451c3b3efca20
lcdlima <leticia.duarte.lima@gmail.com>
2020-06-02 16:24:17 -0300

    Adding tags thead and tbody in the table

--- src/components/Table.js
+++ src/components/Table.js
@@ -19,24 +19,28 @@ class Table extends Component {
     if (results) {
       return (
         <table>
-          <tr>
-            <th>Name</th>
-            <th>Rotation Period</th>
-            <th>Orbital Period</th>
-            <th>Diameter</th>
-            <th>Climate</th>
-            <th>Gravity</th>
-            <th>Terrain</th>
-            <th>Surface Water</th>
-            <th>Population</th>
-            <th>Films</th>
-            <th>Created</th>
-            <th>Edited</th>
-            <th>URL</th>
-          </tr>
-          {results.map((planet) =>
-            <PlanetLine planet={planet} key={planet.name} />,
-          )}
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Rotation Period</th>
+              <th>Orbital Period</th>
+              <th>Diameter</th>
+              <th>Climate</th>
+              <th>Gravity</th>
+              <th>Terrain</th>
+              <th>Surface Water</th>
+              <th>Population</th>
+              <th>Films</th>
+              <th>Created</th>
+              <th>Edited</th>
+              <th>URL</th>
+            </tr>
+          </thead>
+          <tbody>
+            {results.map((planet) =>
+              <PlanetLine planet={planet} key={planet.name} />,
+            )}
+          </tbody>
         </table>
       );
     }
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,9 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
-
 import { combineReducers } from 'redux';
-
 import data from '../reducers/index';
 
-const rootReducer = combineReducers({
-  data,
-});
+const rootReducer = combineReducers({ data });
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
