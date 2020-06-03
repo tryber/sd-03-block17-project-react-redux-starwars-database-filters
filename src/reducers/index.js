b6d586d53907aed0a0abeb214d208481d47ff210
Wenderson belko <57719936+wendersonbelko@users.noreply.github.com>
2020-06-03 18:14:44 -0300

    começando do zero

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,35 @@
-function emptyReducer() {
-  return {};
+import { REQUEST_PLANETS, RECEIVE_PLANETS, FILTER_BY_NAME } from '../actions';
+
+const INITIAL_STATE = {
+  isFetching: false,
+  filters: {},
+  data: [],
+  dataFiltered: [],
+};
+
+function PlanetsReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case REQUEST_PLANETS:
+      return {
+        ...state,
+        isFetching: true,
+      };
+    case RECEIVE_PLANETS:
+      return {
+        ...state,
+        isFetching: false,
+        data: action.data.results,
+        dataFiltered: action.data.results,
+      };
+    case FILTER_BY_NAME:
+      return {
+        ...state,
+        filters: { filterByName: { name: action.name } },
+        dataFiltered: state.data.filter((planet) => planet.name.includes(action.name)),
+      };
+    default:
+      return state;
+  }
 }
 
-export default emptyReducer;
+export default PlanetsReducer;
