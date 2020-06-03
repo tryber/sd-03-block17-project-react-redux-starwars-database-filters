0d70a9e1a9571e17b3a4588226af5f881d611ab3
Marco Barbosa <marco.meireles.b@gmail.com>
2020-06-03 19:33:13 -0300

    refatorando menu dropdown

--- src/components/NumericFilter.js
+++ src/components/NumericFilter.js
@@ -36,9 +36,10 @@ class NumericFilter extends React.Component {
         <label htmlFor="column-filter">Filtre por coluna</label>
         <select
           data-testid="column-filter" name="column-filter"
-          onMouseOver={() => columnOptions.map((e) => <option value={e}>{e}</option>)}
           onChange={(e) => this.handleChange('column', e.target.value)}
-        />
+        >
+          {columnOptions.map((e) => <option value={e}>{e}</option>)}
+        </select>  
         <label htmlFor="comparison-filter">Condição</label>
         <select
           data-testid="comparison-filter" name="comparison-filter"
