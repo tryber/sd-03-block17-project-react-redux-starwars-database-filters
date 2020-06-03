e84933e4df414bd6a0fe6cfcb45be0581c992bf8
Clayton Pereira <cpereiramt@gmail.com>
2020-06-03 15:57:41 -0400

    Salvando alteraçoes requisito 3

---
+++ src/components/FilterNumeric.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+export class FilterNumeric extends Component {
+  render() {
+    return (
+      <div>
+        <select data-testid="column-filter" name="filter">
+          <option value="population">population</option>
+          <option value="orbital_period">orbital_period</option>
+          <option value="diameter">diameter</option>
+          <option value="rotation_period">rotation_period</option>
+          <option value="surface_water">surface_water</option>
+        </select>
+        <select data-testid="comparison-filter" name="comparation">
+          <option value="Maior que">Maior que</option>
+          <option value="Menor que">Menor que</option>
+          <option value="diameter">Igual a</option>
+        </select>
+
+        <input type="number" placeholder="insira uma valor" name="input-value" />
+        <button type="button" data-testid="button-filter"> Filtrar</button>
+      </div>
+    );
+  }
+}
+
+export default FilterNumeric;
