af6c189c9992f5f9c4d4cc528b3e2ce407c53182
Rhian Castro <rhi.castro@gmail.com>
2020-06-03 17:13:09 -0300

    lol

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
-function emptyReducer() {
-  return {};
-}
+import { combineReducers } from 'redux';
+import infoPlanet from './infoPlanet';
 
-export default emptyReducer;
+const rootReducer = combineReducers({ infoPlanet });
+
+export default rootReducer;
