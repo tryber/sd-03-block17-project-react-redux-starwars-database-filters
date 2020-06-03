86c35219c8abd49e61269a105a3a47d40e095b77
Clayton Pereira <cpereiramt@gmail.com>
2020-06-03 17:24:15 -0400

    Salvando alteraçoes requisito 3

--- src/action/index.js
+++ src/action/index.js
@@ -1,6 +1,11 @@
 export const requestData = () => ({ type: 'REQUEST_DATA' });
 export const receiveData = (data) => ({ type: 'RECEIVE_DATA', data });
 export const filterPlanet = (filter) => ({ type: 'FILTER_PLANET_DATA', filters: { filterByName: { name: filter } } });
+export const filterNumeric = (column, comparison, value ) => ({
+  type: 'FILTER_PLANET_NUMERIC',
+  filterByNumericValues: [
+    { column, comparison, value }],
+});
 
 export function fetchData() {
   return (dispatch) => {
