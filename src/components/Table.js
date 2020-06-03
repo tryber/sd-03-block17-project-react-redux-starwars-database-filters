1508996d961578a6c325bc85a86f0ccb9bbb184e
Lourenço <loulage@gmail.com>
2020-06-03 14:35:41 -0300

    cc fixes

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import ApiRequestComp from './ApiRequestComp';
+import TableWithData from './TableWithData';
 
-const Table = () => <div>StarWars Datatable with Filters</div>;
+export class Table extends Component {
+  render() {
+    const { data } = this.props;
+    return (
+      <table>
+        <ApiRequestComp />
+        <tbody>
+          <tr className="table-headers">
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+          </tr>
+        </tbody>
+        <TableWithData data={data} />
+      </table>
+    );
+  }
+}
 
-export default Table;
+const mapStateToProps = (state) => ({
+  data: state.apiReducer.data,
+});
+export default connect(mapStateToProps)(Table);
--- src/services/SWPlanetApiRequest.js
+++ src/services/SWPlanetApiRequest.js
@@ -1,8 +1,7 @@
 const apiUrl = 'http https://swapi-trybe.herokuapp.com/api/planets';
-const SWPlanetApiRequest = () => {
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => (response.ok ? Promise.resolve(data) : Promise.reject(data));
-};
+const SWPlanetApiRequest = () => fetch(apiUrl).then((response) => (
+  response.json()
+    .then((data) => (response.ok ? Promise.resolve(data) : Promise.reject(data)))
+));
 
 export default SWPlanetApiRequest;
