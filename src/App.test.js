92378db63256c83c08901bd4495661fbc2e10a87
Rhian Castro <rhi.castro@gmail.com>
2020-06-03 15:04:27 -0300

    Revert "Fix field to look at the test"
    This reverts commit 1f43274650bc8545fca526faf2fde95f8d4275f6.

--- src/App.test.js
+++ src/App.test.js
@@ -118,11 +118,11 @@ describe('2 - Sua página deve ter um campo de texto que filtra a tabela para so
     const { findByTestId } = renderApp();
     const filterField = await findByTestId('name-filter');
     fireEvent.change(filterField, {target: { value: 'o' }});
-    expect(store.getState().filters.filterByName.name).toEqual('o');
+    expect(store.getState().filters.filterByName.value).toEqual('o');
     fireEvent.change(filterField, {target: { value: 'oo' }});
-    expect(store.getState().filters.filterByName.name).toEqual('oo');
+    expect(store.getState().filters.filterByName.value).toEqual('oo');
     fireEvent.change(filterField, {target: { value: '' }});
-    expect(store.getState().filters.filterByName.name).toEqual('');
+    expect(store.getState().filters.filterByName.value).toEqual('');
   })
 })
 
