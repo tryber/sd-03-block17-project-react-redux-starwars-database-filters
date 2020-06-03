89f47c6e76916093fa7c952d189ccd8a13b48833
Gabriel <dreadpool38@gmail.com>
2020-06-03 11:11:32 -0300

    ADD:Filters

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,30 +2,91 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { object } from 'prop-types';
 
+const head = [
+  'name',
+  'rotation_period',
+  'orbital_period',
+  'diameter',
+  'climate',
+  'gravity',
+  'terrain',
+  'surface_water',
+  'population',
+  'residents',
+  'films',
+  'created',
+  'edited',
+  'url',
+];
+
 class Table extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-
-    };
+    this.state = {};
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
+    console.log('mont');
     const { getPlanets } = this.props;
     getPlanets();
   }
 
+  handleChange(e) {
+    const { setFilter } = this.props;
+    setFilter(e.target.value);
+  }
+
+  filterData(data) {
+    const { filters: { filterByName } } = this.props;
+    let newData;
+
+    if (data.results.length && filterByName.name) {
+      newData = {
+        ...data,
+        results: data.results.filter((planet) => planet.name.includes(filterByName.name)),
+      };
+      return newData;
+    }
+
+    return data;
+  }
+
   render() {
     const { data } = this.props;
-    data && console.log(Object.keys(data.results[0]));
+    const {
+      filters: { filterByName },
+    } = this.props;
+
+    const filteredData = this.filterData(data);
+
     return (
-      <table>
-        <tr>
-          {data && Object.keys(data.results[0]).map((header) => <th key={header}>{header}</th>)}
-        </tr>
-        {data && Object.entries(data.results[0]).map((header) => (header[0] === 'residents' ? <th>Gabriel</th> : <th key={header}>{header[1]}</th>))}
-        <tr />
-      </table>
+      <>
+        <input
+          value={filterByName.name}
+          type="text"
+          onChange={this.handleChange}
+        />
+        <table>
+          <thead>
+            <tr>
+              {head.map((header) => (<th key={header}>{header}</th>))}
+            </tr>
+          </thead>
+
+          <tbody>
+            {filteredData.results.map((planet) => (
+              <tr key={planet.name}>
+                {Object.entries(planet).map((header) => (header[0] === 'residents' ? (
+                  <td key={header}>null </td>
+                ) : (
+                  <td key={header}>{header[1]}</td>
+                )))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
     );
   }
 }
@@ -35,13 +96,15 @@ function mapDispatch(dispatch) {
     getPlanets: () => fetch('https://swapi-trybe.herokuapp.com/api/planets/')
       .then((r) => r.json())
       .then((r) => dispatch({ type: 'API_CALL', r })),
+
+    setFilter: (filter) => dispatch({ type: 'SET_FILTER', filter }),
   };
 }
 function mapState(state) {
   return {
     data: state.data,
+    filters: state.filters,
   };
 }
-
 
 export default connect(mapState, mapDispatch)(Table);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,16 @@
 
-const initialState = {};
+const initialState = {
+  data: { results: [] },
+  filters: { filterByName: { name: '' } },
+};
 
 function emptyReducer(state = initialState, action) {
   switch (action.type) {
     case 'API_CALL':
       return { ...state, data: action.r };
+
+    case 'SET_FILTER':
+      return { ...state, filters: { filterByName: { name: action.filter } } };
 
 
     default:
