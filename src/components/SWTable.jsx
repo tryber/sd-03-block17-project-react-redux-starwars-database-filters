9c50f71755fc835042c8ede55f301a86ac2ef73e
DevT <aguiarthiag@gmail.com>
2020-06-03 14:37:42 -0300

    Iniciando o projeto

---
+++ src/components/SWTable.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SWTable extends Component {
+  render() {
+    return (
+      <div>StarWars Table</div>
+    )
+  }
+}
+
+export default SWTable;
