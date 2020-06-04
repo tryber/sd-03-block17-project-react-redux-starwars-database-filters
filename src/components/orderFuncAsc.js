78b1315cce24fe2c7f84610fd27ec45df3847911
luis.campos <gucampos845@gmail.com>
2020-06-04 19:09:46 -0300

    CodeClimate 5

---
+++ src/components/orderFuncAsc.js
@@ -0,0 +1,18 @@
+import filterFunc from './filterFunc';
+
+const orderFuncAsc = (planets, name, numericValues, columnSort, sort) => {
+  if (columnSort === 'Name') return filterFunc(planets, name, numericValues)
+    .sort((a, b) => a.name - b.name);
+  return filterFunc(planets, name, numericValues).sort((a, b) => a[columnSort] - b[columnSort]);
+};
+
+export default orderFuncAsc;
+
+/* return ((columnSort === 'Name') &&
+    filterFunc(planets, name, numericValues).sort((a, b) => (
+      sort === 'ASC' ? a.name - b.name : b.name - a.name
+    )))
+    ||
+    filterFunc(planets, name, numericValues).sort((a, b) => (
+      sort === 'ASC' ? a[columnSort] - b[columnSort] : b[columnSort] - a[columnSort]
+    )); */
