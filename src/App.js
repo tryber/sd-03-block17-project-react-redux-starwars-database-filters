00668a84d49caca93a07d5e96849ed8ac9968511
rsfdev <roger_faustino@hotmail.com>
2020-06-04 16:06:02 -0300

    Starting the project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import planetsFetch from './actions/planetsFetch';
+import Filters from './components/Filters';
 import './App.css';
-
 import Table from './components/Table';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends Component {
+  componentDidMount() {
+    const { fetch } = this.props;
+    fetch();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Filters />
         <Table />
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
-export default App;
+App.propTypes = {
+  fetch: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators({ fetch: planetsFetch }, dispatch);
+
+export default connect(null, mapDispatchToProps)(App);
