78d75ba04a4341e4aa01c6fcd4d20aa7a651c096
Hfreitas <hfreitas86@gmail.com>
2020-06-04 18:10:33 -0300

    refatorei e já sei que não passa no code climate

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,83 +1,74 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { filterByText } from '../actions/actionsCreators';
 import Table from '../components/table/Table';
 import FilterContainer from '../components/filters/FilterContainer';
 
-class Home extends Component {
-  static makeComparison(column, comparison, value, element) {
-    switch (comparison) {
-      case 'maior que':
-        return Number(element[column]) > Number(value);
-      case 'menor que':
-        return Number(element[column]) < Number(value);
-      case 'igual a':
-        return Number(element[column]) === Number(value);
-      default:
-        return [];
-    }
-  }
-
-  static orderColumns(data, column, order) {
-    const integersColumns = [
-      'population',
-      'orbital_period',
-      'diameter',
-      'rotation_period',
-      'surface_water',
-    ];
-    const sortedData = integersColumns.includes(column)
-      ? data.sort((elemA, elemB) => elemA[column] - elemB[column])
-      : data.sort((elemA, elemB) => elemA[column].localeCompare(elemB[column]));
-
-    if (order === 'DESC') sortedData.reverse();
-    return sortedData;
-  }
-
-  filterDataByText(data) {
-    const { nameFilter } = this.props;
-    if (nameFilter !== '') {
-      return data.filter(({ name }) => name.toLowerCase().includes(nameFilter.toLowerCase()));
-    }
-    return data;
-  }
-
-  sortDataFilter(data) {
-    const { sortColumnFilter, sortOrderFilter } = this.props;
-    return Home.orderColumns(
-      this.filterDataByText(data),
-      sortColumnFilter.toLowerCase(),
-      sortOrderFilter,
-    );
-  }
-
-  filterDataByNumericValue(data) {
-    const { valueFilters } = this.props;
-    if (valueFilters) {
-      return valueFilters.reduce(
-        (acc, { column, comparison, value }) => acc
-          .filter((element) => Home.makeComparison(column, comparison, value, element)),
-        this.sortDataFilter(data),
-      );
-    }
-    return this.sortDataFilter(data);
-  }
-
-  render() {
-    const { data, planetName } = this.props;
-    return (
-      <div>
-        <div>
-          <FilterContainer
-            onChange={(event) => planetName(event.target.value)}
-          />
-        </div>
-        <Table data={this.filterDataByNumericValue(data)} />
-      </div>
-    );
+function makeComparison(column, comparison, value, element) {
+  switch (comparison) {
+    case 'maior que':
+      return Number(element[column]) > Number(value);
+    case 'menor que':
+      return Number(element[column]) < Number(value);
+    case 'igual a':
+      return Number(element[column]) === Number(value);
+    default:
+      return [];
   }
 }
+
+function orderColumns(data, column, order) {
+  const integersColumns = [
+    'population',
+    'orbital_period',
+    'diameter',
+    'rotation_period',
+    'surface_water',
+  ];
+  const sortedData = integersColumns.includes(column)
+    ? data.sort((elemA, elemB) => elemA[column] - elemB[column])
+    : data.sort((elemA, elemB) => elemA[column].localeCompare(elemB[column]));
+
+  if (order === 'DESC') sortedData.reverse();
+  return sortedData;
+}
+
+const Home = ({
+  nameFilter,
+  sortColumnFilter,
+  sortOrderFilter,
+  valueFilters,
+  data,
+  planetName,
+}) => {
+  const filterDataByText = (nameFilter !== ''
+    ? data.filter(({ name }) => name.toLowerCase().includes(nameFilter.toLowerCase()))
+    : data);
+
+  const sortDataFilter = orderColumns(
+    filterDataByText,
+    sortColumnFilter.toLowerCase(),
+    sortOrderFilter,
+  );
+
+  const filterDataByNumericValue = (valueFilters
+    ? valueFilters.reduce(
+      (acc, { column, comparison, value }) => acc
+        .filter((element) => makeComparison(column, comparison, value, element)),
+      sortDataFilter,
+    )
+    : sortDataFilter);
+
+  return (
+    <div>
+      <div>
+        <FilterContainer onChange={(event) => planetName(event.target.value)} />
+      </div>
+      <Table data={filterDataByNumericValue} />
+    </div>
+  );
+};
 
 const mapDispatchToProps = (dispatch) => ({
   planetName: (planetName) => dispatch(filterByText(planetName)),
