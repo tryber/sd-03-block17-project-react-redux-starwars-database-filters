76aa779dbebbdf780cdc4b81fde42769b1cb788d
nato-x <alvares.renato@hotmail.com>
2020-06-04 16:04:14 -0300

    cc]

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import propTypes, { arrayOf } from 'prop-types';
+import propTypes from 'prop-types';
 import { handleFetch } from '../actions/fetchActions';
 
 import TableHeaders from './TableHeaders';
