6cb19f60509a04a4e9584abcea76fbd74835735f
luis.campos <gucampos845@gmail.com>
2020-06-04 19:12:06 -0300

    CodeClimate 6

--- src/components/orderFuncAsc.js
+++ src/components/orderFuncAsc.js
@@ -1,8 +1,10 @@
 import filterFunc from './filterFunc';
 
-const orderFuncAsc = (planets, name, numericValues, columnSort, sort) => {
-  if (columnSort === 'Name') return filterFunc(planets, name, numericValues)
-    .sort((a, b) => a.name - b.name);
+const orderFuncAsc = (planets, name, numericValues, columnSort) => {
+  if (columnSort === 'Name') {
+    return filterFunc(planets, name, numericValues)
+      .sort((a, b) => a.name - b.name);
+  }
   return filterFunc(planets, name, numericValues).sort((a, b) => a[columnSort] - b[columnSort]);
 };
 
--- src/components/orderFuncDesc.js
+++ src/components/orderFuncDesc.js
@@ -1,8 +1,10 @@
 import filterFunc from './filterFunc';
 
 const orderFuncDesc = (planets, name, numericValues, columnSort) => {
-  if (columnSort === 'Name') return filterFunc(planets, name, numericValues)
-    .sort((a, b) => b.name - a.name);
+  if (columnSort === 'Name') {
+    return filterFunc(planets, name, numericValues)
+      .sort((a, b) => b.name - a.name);
+  }
   return filterFunc(planets, name, numericValues).sort((a, b) => b[columnSort] - a[columnSort]);
 };
 
