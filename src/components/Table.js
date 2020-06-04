96d6459f7a8ba6523003a97875303db11a828beb
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-06-03 21:22:00 -0300

    Requisito 3 em produção

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,6 +5,27 @@ import '../App.css';
 
 
 class Table extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      titles: [
+        { id: 1, title: 'name' },
+        { id: 2, title: 'climate' },
+        { id: 3, title: 'created' },
+        { id: 4, title: 'diameter' },
+        { id: 5, title: 'edited' },
+        { id: 6, title: 'films' },
+        { id: 7, title: 'gravity' },
+        { id: 8, title: 'orbital period' },
+        { id: 9, title: 'population' },
+        { id: 10, title: 'residents' },
+        { id: 11, title: 'rotation period' },
+        { id: 12, title: 'surface water' },
+        { id: 13, title: 'terrain' },
+      ],
+    };
+  }
+
   tbody(data, name) {
     return (
       <tbody>
@@ -78,50 +99,6 @@ class Table extends React.Component {
     return (
       <tbody>
         {data.filter((item) => item.name.includes(name) && parseInt(item[option]) === parseInt(valueFilter))
-          .map((item) => <tr key={item.name}>
-            <td>{item.name}</td>
-            <td>{item.climate}</td>
-            <td>{item.created}</td>
-            <td>{item.diameter}</td>
-            <td>{item.edited}</td>
-            <td>#</td>
-            <td>{item.gravity}</td>
-            <td>{item.orbital_period}</td>
-            <td>{item.population}</td>
-            <td>#</td>
-            <td>{item.rotation_period}</td>
-            <td>{item.surface_water}</td>
-            <td>{item.terrain}</td>
-          </tr>)}
-      </tbody>
-    );
-  }
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      titles: [
-        { id: 1, title: 'name' },
-        { id: 2, title: 'climate' },
-        { id: 3, title: 'created' },
-        { id: 4, title: 'diameter' },
-        { id: 5, title: 'edited' },
-        { id: 6, title: 'films' },
-        { id: 7, title: 'gravity' },
-        { id: 8, title: 'orbital period' },
-        { id: 9, title: 'population' },
-        { id: 10, title: 'residents' },
-        { id: 11, title: 'rotation period' },
-        { id: 12, title: 'surface water' },
-        { id: 13, title: 'terrain' },
-      ],
-    };
-  }
-
-  tbody(data, name) {
-    return (
-      <tbody>
-        {data.filter((item) => item.name.includes(name))
           .map((item) => <tr key={item.name}>
             <td>{item.name}</td>
             <td>{item.climate}</td>
