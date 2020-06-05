fa5e026138d04c5fdf0f2a3f09f7939c77eba934
William Igor <williamigorsilva@gmail.com>
2020-06-05 18:38:35 -0300

    PropTypes

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -67,24 +67,9 @@ const mapStateToProps = (state) => ({
   selectInput: state.filters.filterByNumericValues,
 });
 
+
 Table.propTypes = {
-  planets: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string,
-      rotation_period: PropTypes.string,
-      orbital_period: PropTypes.string,
-      diameter: PropTypes.string,
-      climate: PropTypes.string,
-      gravity: PropTypes.string,
-      terrain: PropTypes.string,
-      surface_water: PropTypes.string,
-      population: PropTypes.string,
-      film: PropTypes.string,
-      created: PropTypes.string,
-      edited: PropTypes.string,
-      url: PropTypes.string,
-    }),
-  ).isRequired,
+  planets: PropTypes.arrayOf(PropTypes.object).isRequired,
   isFetching: PropTypes.bool.isRequired,
   filterByName: PropTypes.string.isRequired,
   selectInput: PropTypes.arrayOf(
