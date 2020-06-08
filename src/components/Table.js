1b96fce743c5a4f7c5661315a982dd70ef483e24
cpereira <cpereiramt@gmail.com>
2020-06-08 18:45:35 -0400

    Revert "Salvando alterações nos requisitos 2 e 3"
    This reverts commit d074dee71e32ebbbe0a17e00dc0d17f1a35ec92b.

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,11 +3,65 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 export class Table extends Component {
-  renderTableBody() {
+  this.filtraData = this.filtraData.bind(this);
+  this.filterDataByName = this.filterDataByName.bind(this);
+  this.renderTableBody = this.renderTableBody.bind(this);
+  this.fetchUrl = this.fetchUrl.bind(this);
+}
+
+componentDidMount() {
+  this.fetchUrl();
+}
+
+fetchUrl() {
+  const { request } = this.props;
+  request();
+  console.log(this.props.value)
+}
+
+retornaSign(comparison) {
+  let comparisonSignal;
+  if (comparison === 'maior que') {
+    comparisonSignal = 0;
+    return comparisonSignal;
+  } if (comparison === 'menor que') {
+    comparisonSignal = 1;
+    return comparisonSignal;
+  } if (comparison === 'igual a') {
+    comparisonSignal = 2;
+    return comparisonSignal;
+  }
+  return null;
+}
+
+filtraData(comparisonSignal, results, column, value, name) {
+  if (comparisonSignal === 0) {
+    return results.filter((element) => element[column] > value);
+
+  } if (comparisonSignal === 1) {
+    return results.filter((element) => element[column] < value);
+  } if (comparisonSignal === 2) {
+    return results.filter((element) => element[column] === value);
+  } if (name !== ' ') {
+    console.log("filtername")
+   return this.filterDataByName(name, results)
+  } if (comparisonSignal = null && name === ' ') {
+    return results;
+  }
+  return results;
+}
+
+filterDataByName(filtername, results) {
+      return results.filter((element) => {
+      const lowerName = element.name.toLowerCase();
+      return lowerName.includes(filtername);
+    })
+}
+
+  renderTableBody(filtername, filterNumb) {
     const { value: { data } } = this.props;
     const { results } = data;
     return results.map((element) => (
-      <tr key={element.name}>
         <td>{element.name}</td>
         <td>{element.rotation_period}</td>
         <td>{element.orbital_period}</td>
@@ -51,6 +105,9 @@ export class Table extends Component {
     const { value: { data } } = this.props;
     const { value: { filters: { filterByName: { filteredPlanets } } } } = this.props;
     const { results } = data;
+    if(this.filteredPlanets === undefined ) {
+    this.filteredPlanets = results;
+    console.log(this.filteredPlanets)}
     const headers = ['name', 'rotation_period', 'orbital_period', 'diameter', 'climate', 'gravity', 'terrain', 'surface_water', 'population', 'films', 'created', 'edited', 'url'];
     return (
       <div>
