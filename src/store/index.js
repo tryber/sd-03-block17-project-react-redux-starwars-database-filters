fe25c9680d982216ac240cbdbc56aab29537fc21
cpereira <cpereiramt@gmail.com>
2020-06-09 18:33:05 -0400

    Salvando alterações

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,20 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import requestReducer from '../reducers';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import requestReducer from "../reducers";
 
-const composeMiddlewares = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(requestReducer, composeMiddlewares(applyMiddleware(thunk)));
-
+const composeMiddlewares =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    requestReducer,
+    composeMiddlewares(applyMiddleware(thunk))
+);
 
 export default store;
 
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import requestReducer from '../reducers';
-
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import requestReducer from "../reducers";
 
 const store = createStore(requestReducer, applyMiddleware(thunk));
 
-
-export default store; 
+export default store;
