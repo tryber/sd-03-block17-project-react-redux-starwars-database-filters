67c7d45424e6e194bd07b894bd678e8e42f9de68
henrique <hbaetaleite@gmail.com>
2020-06-09 19:23:29 -0300

    Criando pastas

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
-const Table = () => <div>StarWars Datatable with Filters</div>;
+const Table = () => (
+  <div>
+    StarWars Datatable with Filters
+    </div>
+);
 
 export default Table;
