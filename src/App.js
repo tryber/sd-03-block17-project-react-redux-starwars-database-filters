2c018c688de1c6f388d7f00c25811020a6e79434
renan.andreolle <renandreolle@hotmail.com>
2020-06-12 13:08:26 -0300

    Zona2

--- src/App.js
+++ src/App.js
@@ -1,43 +1,15 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import Home from './components/Home';
 import './App.css';
-import Table from './components/Table';
-import Input from './components/Input';
-import Remove from './components/Remove';
-import { requestFetch } from './actions/actions';
 
-export class App extends React.Component {
-  componentDidMount() {
-    const { getPlanets } = this.props;
-    getPlanets();
-  }
-
-  render(){
-    if (this.props.loading) return <h1>Loading...</h1>;
-    else return (
+class App extends React.Component {
+  render() {
+    return (
       <div className="App">
-        <header className="App-header">
-          <Input />
-          <Remove />
-          <Table />
-       </header>
-     </div>
+        <Home />
+      </div>
     );
   }
 }
 
-const mapStateToProps = (state) => ({
-  loading: state.getPlanets.isFetching,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  getPlanets: () => dispatch(requestFetch()),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
-
-App.propTypes = {
-  getPlanets: PropTypes.func.isRequired,
-  loading: PropTypes.bool.isRequired,
-};
+export default App;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,117 +1,17 @@
+
 import React from 'react';
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux';
-import orderFuncAsc from './orderFuncAsc';
-import orderFuncDesc from './orderFuncDesc';
+import Head from './Head';
+import Body from './Body';
 
-
-function Table({ planets, name, numericValues, columnSort, sort }) {
-  const data = sort === 'ASC' ? orderFuncAsc(planets, name, numericValues, columnSort) :
-    orderFuncDesc(planets, name, numericValues, columnSort);
-  function head(){
-    return (
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Rotation Period</th>
-          <th>Orbital Period</th>
-          <th>Diameter</th>
-          <th>Climate</th>
-          <th>Gravity</th>
-          <th>Terrain</th>
-          <th>Surface Water</th>
-          <th>Population</th>
-          <th>Film</th>
-          <th>Created</th>
-          <th>Edited</th>
-          <th>Url</th>
-        </tr>
-    </thead>
-    )
-  }
-
-  function body(){
-    return (
-      <tbody>
-        {data.map((planet) => (
-          <tr key={planet.name}>
-            <td>{planet.name}</td>
-            <td>{planet.rotation_period}</td>
-            <td>{planet.orbital_period}</td>
-            <td>{planet.diameter}</td>
-            <td>{planet.climate}</td>
-            <td>{planet.gravity}</td>
-            <td>{planet.terrain}</td>
-            <td>{planet.surface_water}</td>
-            <td>{planet.population}</td>
-            <td>
-              {planet.films.map((film) => (
-                <span key={film}>{film}</span>
-              ))}
-            </td>
-            <td>{planet.created}</td>
-            <td>{planet.edited}</td>
-            <td>{planet.url}</td>
-          </tr>
-        ))}
-      </tbody>
-    );
-  }
-
+function Table() {
   return (
-    <div>
-      <table>
-        {head()}
-        {body()}
+    <div className="container">
+      <table className="table is-hoverable">
+        <Head />
+        <Body />
       </table>
-    </div>
-  )
-  
+    </div >
+  );
 }
 
-const mapStateToProps = (state) => ({
-  planetas: state.getPlanets.data,
-  name: state.filters.filterByName.name,
-  numericValues: state.filters.filterByNumericValues,
-  columnSort: state.filters.order.column,
-  sort: state.filters.order.sort,
-});
-
-Table.defaultProps = {
-  columnSort: 'Name',
-  sort: 'ASC',
-};
-
-Table.propTypes = {
-  planetas: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string,
-      rotation_period: PropTypes.string,
-      orbital_period: PropTypes.string,
-      diameter: PropTypes.string,
-      climate: PropTypes.string,
-      gravity: PropTypes.string,
-      terrain: PropTypes.string,
-      surface_water: PropTypes.string,
-      population: PropTypes.string,
-      film: PropTypes.string,
-      created: PropTypes.string,
-      edited: PropTypes.string,
-      url: PropTypes.string,
-    })
-  ),
-  name: PropTypes.string.isRequired,
-  numericValues: PropTypes.arrayOf(
-    PropTypes.shape({
-      column: PropTypes.string,
-      comparison: PropTypes.string,
-      value: PropTypes.string,
-      columnSort: PropTypes.string,
-      sort: PropTypes.string,
-    }),
-  ).isRequired,
-  columnSort: PropTypes.string.isRequired,
-  sort: PropTypes.string.isRequired,
-}
-
-export default connect(mapStateToProps)(Table);
+export default Table;
