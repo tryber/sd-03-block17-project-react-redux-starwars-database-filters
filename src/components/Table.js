58c7d060b4fe33f658ce9a056d32c916c2982442
mirellaalves <mirella.alves@gmail.com>
2020-06-13 22:09:26 -0300

    Correcting syntax.

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,61 +5,66 @@ import PropTypes from 'prop-types';
 import { receiveStarWarsSuccess } from '../actions';
 
 class Table extends Component {
+
   componentDidMount() {
     const { getStarWarsPlanetsData } = this.props;
     getStarWarsPlanetsData();
   }
 
   tableHead() {
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Rotation Period</th>
-        <th>Orbital Period</th>
-        <th>Diameter</th>
-        <th>Climate</th>
-        <th>Gravity</th>
-        <th>Terrain</th>
-        <th>Surface Water</th>
-        <th>Population</th>
-        <th>Films</th>
-        <th>Created</th>
-        <th>Edited</th>
-        <th>Url</th>
-      </tr>
-    </thead>
+    return (
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Rotation Period</th>
+          <th>Orbital Period</th>
+          <th>Diameter</th>
+          <th>Climate</th>
+          <th>Gravity</th>
+          <th>Terrain</th>
+          <th>Surface Water</th>
+          <th>Population</th>
+          <th>Films</th>
+          <th>Created</th>
+          <th>Edited</th>
+          <th>Url</th>
+        </tr>
+      </thead>
+    );
   }
 
   tableBody() {
-    <tbody>
-      {data.map((planet) => (
-        <tr key="planet.name">
-          <td>{planet.name}</td>
-          <td>{planet.rotation_period}</td>
-          <td>{planet.orbital_period}</td>
-          <td>{planet.diameter}</td>
-          <td>{planet.climate}</td>
-          <td>{planet.gravity}</td>
-          <td>{planet.terrain}</td>
-          <td>{planet.surface_water}</td>
-          <td>{planet.population}</td>
-          <td>{planet.films}</td>
-          <td>{planet.created}</td>
-          <td>{planet.edited}</td>
-          <td>{planet.url}</td>
-        </tr>
-      ))}
-    </tbody>
+    const { data } = this.props;
+    return (
+      <tbody>
+        {data.map((planet) => (
+          <tr key="planet.name">
+            <td>{planet.name}</td>
+            <td>{planet.rotation_period}</td>
+            <td>{planet.orbital_period}</td>
+            <td>{planet.diameter}</td>
+            <td>{planet.climate}</td>
+            <td>{planet.gravity}</td>
+            <td>{planet.terrain}</td>
+            <td>{planet.surface_water}</td>
+            <td>{planet.population}</td>
+            <td>{planet.films}</td>
+            <td>{planet.created}</td>
+            <td>{planet.edited}</td>
+            <td>{planet.url}</td>
+          </tr>
+        ))}
+      </tbody>
+    );
   }
 
   render() {
-    const { data } = this.props;
     return (
       <div>
         <div>StarWars Datatable with Filters</div>
         <table>
           {this.tableHead()}
-          {this.tableBody()}          
+          {this.tableBody()}
         </table>
       </div>
     );
