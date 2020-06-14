208e1b4efe4a48e8f456a646d4d5f88cd153903f
cpereira <cpereiramt@gmail.com>
2020-06-13 20:03:00 -0400

    Refatorando componente Table

--- src/components/Table.js
+++ src/components/Table.js
@@ -25,32 +25,13 @@ export class Table extends Component {
     );
   }
 
-  static subSelectRender(filters, value, data) {
-    let planets;
-    if (filters.filterByName.name === '' && filters.filterByNumericValues.length === 0) {
-      planets = data.results;
-    } else {
-      planets = filtraData(data.results,
-        value.filters.filterByName.name,
-        value.filters.filterByNumericValues);
-    }
-    return planets;
-  }
-
-  static selectRender(value, data) {
-    let planets;
-    if (value.isLoading !== undefined && value.isLoading === false) {
-      const { filters } = value;
-      planets = this.subSelectRender(filters, value, data);
-    }
-    return planets;
-  }
-
-
   render() {
     const { value } = this.props;
     const { data } = value;
-    const planets = Table.selectRender(value, data);
+    console.log(value);
+    const planets = filtraData(data.results,
+      value.filters.filterByName.name,
+      value.filters.filterByNumericValues);
     const headers = ['name', 'rotation_period', 'orbital_period', 'diameter', 'climate', 'gravity', 'terrain', 'surface_water', 'population', 'films', 'created', 'edited', 'url'];
     return (
       <div>
