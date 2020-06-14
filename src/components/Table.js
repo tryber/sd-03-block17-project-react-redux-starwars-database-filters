2e09f7dbfde01011a8f373adc8161336018d8528
mirellaalves <mirella.alves@gmail.com>
2020-06-13 22:32:02 -0300

    Code climate issues.

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import { receiveStarWarsSuccess } from '../actions';
+import TableHead from './TableHead';
 
 class Table extends Component {
   componentDidMount() {
@@ -11,30 +12,32 @@ class Table extends Component {
   }
 
   render() {
+    const { data } = this.props;
+    
     return (
       <div>
         <div>StarWars Datatable with Filters</div>
         <table>
           <TableHead />
-            <tbody>
-              {data.map((planet) => (
-                <tr key="planet.name">
-                  <td>{planet.name}</td>
-                  <td>{planet.rotation_period}</td>
-                  <td>{planet.orbital_period}</td>
-                  <td>{planet.diameter}</td>
-                  <td>{planet.climate}</td>
-                  <td>{planet.gravity}</td>
-                  <td>{planet.terrain}</td>
-                  <td>{planet.surface_water}</td>
-                  <td>{planet.population}</td>
-                  <td>{planet.films}</td>
-                  <td>{planet.created}</td>
-                  <td>{planet.edited}</td>
-                  <td>{planet.url}</td>
-                </tr>
-              ))}
-            </tbody>
+          <tbody>
+            {data.map((planet) => (
+              <tr key="planet.name">
+                <td>{planet.name}</td>
+                <td>{planet.rotation_period}</td>
+                <td>{planet.orbital_period}</td>
+                <td>{planet.diameter}</td>
+                <td>{planet.climate}</td>
+                <td>{planet.gravity}</td>
+                <td>{planet.terrain}</td>
+                <td>{planet.surface_water}</td>
+                <td>{planet.population}</td>
+                <td>{planet.films}</td>
+                <td>{planet.created}</td>
+                <td>{planet.edited}</td>
+                <td>{planet.url}</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     );
