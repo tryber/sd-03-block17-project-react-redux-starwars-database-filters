70370f405e9aa0da4ab0d27639f5f05321e7c758
henrique <hbaetaleite@gmail.com>
2020-06-17 17:38:19 -0300

    Criando action index.js

---
+++ src/actions/index.js
@@ -0,0 +1,27 @@
+import fetchAPI from '../services/starWarsAPI';
+import { REQUEST_API, RECEIVE_API_SUCCESS, RECEIVE_API_FAILURE } from './types';
+
+const requestPlanets = () => ({
+  type: REQUEST_API,
+});
+
+const receiveAPISuccess = ({ results }) => ({
+  type: RECEIVE_API_SUCCESS,
+  payload: results,
+});
+
+const receiveAPIFailure = (error) => ({
+  type: RECEIVE_API_FAILURE,
+  payload: error,
+});
+
+export default function fetchPlanets() {
+  return (dispatch) => {
+    dispatch(requestPlanets());
+    return fetchAPI()
+      .then(
+        (data) => dispatch(receiveAPISuccess(data)),
+        (error) => dispatch(receiveAPIFailure(error.message)),
+      );
+  };
+}
--- src/services/starWarsAPI.js
+++ src/services/starWarsAPI.js
@@ -1,7 +1,11 @@
+import { func } from 'prop-types';
+
 const api = 'https://swapi-trybe.herokuapp.com/api/planets/';
 
-const fetchAPI = () => fetch(api).then((planets) => (planets.json()
-  .then((data) => (planets.ok ? Promise.resolve(data) : Promise.reject(data)))
-));
+export default async function fetchAPI() {
+  return fetch(api).then((planets) =>
+    planets.json().then((data) => (planets.ok ? Promise.resolve(data) : Promise.reject(data))),
+  );
+}
 
 export default fetchAPI;
