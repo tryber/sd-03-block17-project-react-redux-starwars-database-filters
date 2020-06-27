bc6e87b49f2ccd20ebc4ba2c55e73b4fce637b10
William Igor <williamigorsilva@gmail.com>
2020-06-26 22:27:00 -0300

    teste ok

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import './App.css';
 import { Provider } from 'react-redux';
+import './App.css';
 import Table from './components/Table';
 import store from './store/store';
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import store from './store/store';
 
-// // <Provider store={store}>
-// {/* </Provider>, */}
 ReactDOM.render(
   <App />,
   document.getElementById('root'),
